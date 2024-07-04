2feec2705c8fda98cfb5af85a64b297e9fe0c44c
tosiale <156539112+tosiale@users.noreply.github.com>
2024-07-04 23:47:51 +0200

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -1,20 +1,50 @@
 # File: user_manager.py
 
 from dahua import DahuaFunctions
+from utils import log
+from pwdmanager import dahua_gen2_md5_hash
 
 class UserManager:
     def __init__(self, dahua_instance: DahuaFunctions):
         self.dahua = dahua_instance
 
     def add_user(self, username: str, password: str, role: str) -> bool:
-        # Implementation for adding a user
+        # Log the operation
         print(f"Adding user {username} with role {role}")
-        # Example implementation:
-        try:
-            response = self.dahua.add_user(username, password, role)
-            return response
-        except Exception as e:
-            print(f"Error adding user: {e}")
+
+        if not self.dahua.dh_realm:
+            log.failure(f'[add_user] no realm exist ({self.dahua.dh_realm})')
+            return False
+
+        query_args = {
+            'method': 'userManager.getAuthorityList',
+            'params': None,
+        }
+        dh_data = self.dahua.send_call(query_args, error_codes=True)
+        authority_list = dh_data.get('params')
+
+        dh_hash = dahua_gen2_md5_hash(
+            dh_realm=self.dahua.dh_realm, username=username, password=password, return_hash=True
+        )
+
+        query_args = {
+            'method': 'userManager.addUser',
+            'params': {
+                'user': {
+                    'AuthorityList': authority_list,
+                    'Group': role,
+                    'Memo': username,
+                    'Name': username,
+                    'Password': dh_hash,
+                },
+            },
+        }
+        dh_data = self.dahua.send_call(query_args, error_codes=True)
+        if dh_data.get('result'):
+            log.success('Success')
+            return True
+        else:
+            log.failure('Failed: User already exist?')
             return False
 
     def modify_password(self, username: str, new_password: str) -> bool:
